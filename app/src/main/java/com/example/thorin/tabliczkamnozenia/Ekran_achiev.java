0f9701df9221f520cbe61c49e56a86a46b59d36e
Thorin404 <zurichiiii@gmail.com>
2017-02-19 13:42:14 +0100

    drobne poprawki
    opisy poziomów
    możliwość czyszczenia osiągnięć

--- app/src/main/java/com/example/thorin/tabliczkamnozenia/Ekran_achiev.java
+++ app/src/main/java/com/example/thorin/tabliczkamnozenia/Ekran_achiev.java
@@ -2,15 +2,20 @@ package com.example.thorin.tabliczkamnozenia;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.CheckedTextView;
 
 public class Ekran_achiev extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        achievon();
+    }
+
+    private void achievon(){
         setContentView(R.layout.activity_ekran_achiev);
+        final CheckedTextView ctv0= (CheckedTextView)findViewById(R.id.checkedTextView0);
         final CheckedTextView ctv1= (CheckedTextView)findViewById(R.id.checkedTextView1);
         final CheckedTextView ctv2= (CheckedTextView)findViewById(R.id.checkedTextView2);
         final CheckedTextView ctv3= (CheckedTextView)findViewById(R.id.checkedTextView3);
@@ -18,8 +23,7 @@ public class Ekran_achiev extends AppCompatActivity {
         final CheckedTextView ctv5= (CheckedTextView)findViewById(R.id.checkedTextView5);
         final CheckedTextView ctv6= (CheckedTextView)findViewById(R.id.checkedTextView6);
         final CheckedTextView ctv7= (CheckedTextView)findViewById(R.id.checkedTextView7);
-        final CheckedTextView ctv8= (CheckedTextView)findViewById(R.id.checkedTextView8);
-        //pobieranie achievmentów
+        if (single_achiev.achiev0()) ctv0.setChecked(true);
         if (single_achiev.achiev1()) ctv1.setChecked(true);
         if (single_achiev.achiev2()) ctv2.setChecked(true);
         if (single_achiev.achiev3()) ctv3.setChecked(true);
@@ -27,6 +31,30 @@ public class Ekran_achiev extends AppCompatActivity {
         if (single_achiev.achiev5()) ctv5.setChecked(true);
         if (single_achiev.achiev6()) ctv6.setChecked(true);
         if (single_achiev.achiev7()) ctv7.setChecked(true);
-        if (single_achiev.achiev8()) ctv8.setChecked(true);
+    }
+
+    private void achievoff(){
+        setContentView(R.layout.activity_ekran_achiev);
+        final CheckedTextView ctv0= (CheckedTextView)findViewById(R.id.checkedTextView0);
+        final CheckedTextView ctv1= (CheckedTextView)findViewById(R.id.checkedTextView1);
+        final CheckedTextView ctv2= (CheckedTextView)findViewById(R.id.checkedTextView2);
+        final CheckedTextView ctv3= (CheckedTextView)findViewById(R.id.checkedTextView3);
+        final CheckedTextView ctv4= (CheckedTextView)findViewById(R.id.checkedTextView4);
+        final CheckedTextView ctv5= (CheckedTextView)findViewById(R.id.checkedTextView5);
+        final CheckedTextView ctv6= (CheckedTextView)findViewById(R.id.checkedTextView6);
+        final CheckedTextView ctv7= (CheckedTextView)findViewById(R.id.checkedTextView7);
+        if (single_achiev.achiev0()) ctv0.setChecked(false);
+        if (single_achiev.achiev1()) ctv1.setChecked(false);
+        if (single_achiev.achiev2()) ctv2.setChecked(false);
+        if (single_achiev.achiev3()) ctv3.setChecked(false);
+        if (single_achiev.achiev4()) ctv4.setChecked(false);
+        if (single_achiev.achiev5()) ctv5.setChecked(false);
+        if (single_achiev.achiev6()) ctv6.setChecked(false);
+        if (single_achiev.achiev7()) ctv7.setChecked(false);
+    }
+
+    public void sendUsun(View view) {
+        single_achiev.clear();
+        achievoff();
     }
 }
--- app/src/main/java/com/example/thorin/tabliczkamnozenia/single_achiev.java
+++ app/src/main/java/com/example/thorin/tabliczkamnozenia/single_achiev.java
@@ -11,50 +11,64 @@ public class single_achiev {
     }
 
     private static double procent;
+    private static int done[] = {0,0,0,0,0,0,0,0};
 
     //achievement 50%
-    static boolean achiev1() {
-        return procent >= 0.5;
+    static boolean achiev0() {
+        if (procent >= 0.5) done[0] = 1;
+        return done[0] == 1;
     }
 
     //achievement 75%
-    static boolean achiev2() {
-        return procent >= 0.75;
+    static boolean achiev1() {
+        if (procent >= 0.75) done[1] = 1;
+        return done[1] == 1;
     }
 
     //achievement 100%
-    static boolean achiev3() {
-        return procent == 1;
+    static boolean achiev2() {
+        if (procent == 1) done[2] = 1;
+        return done[2] == 1;
     }
 
     //achievement 100% przez 3 dni
-    static boolean achiev4() {
-        return procent == 1;
+    static boolean achiev3() {
+        if (procent == 1) done[3] = 1;
+        return done[3] == 1;
     }
 
     //achievement 100% przez 7 dni
-    static boolean achiev5() {
-        return procent == 1;
+    static boolean achiev4() {
+        if (procent == 1) done[4] = 1;
+        return done[4] == 1;
     }
 
     //achievement 100% przez 14 dni w dwoch grach dziennie
-    static boolean achiev6() {
-        return procent == 1;
+    static boolean achiev5() {
+        if (procent == 1) done[5] = 1;
+        return done[5] == 1;
     }
 
     //achievement 100% przez 30 dni w czterech grach dziennie
-    static boolean achiev7() {
-        return procent == 1;
+    static boolean achiev6() {
+        if (procent == 1) done[6] = 1;
+        return done[6] == 1;
     }
 
     //achievement 44%
-    static boolean achiev8() {
-        return procent == 0.44;
+    static boolean achiev7() {
+        if (procent == 0.44) done[7] = 1;
+        return done[7] == 1;
     }
 
     //pobiera % wygranych
     static void setwin(double a) {
         procent = a;
+    }
+
+    //usuwanie osiagnięć
+    static void clear() {
+        for (int i = 0; i < 8; i++) done[i] = 0;
     }
 
     private single_achiev() {
